0d3e5a46ae497fc9f4b522fc9908a61e97f61119
Cristhian Vera <cristhianverarivas22@gmail.com>
2025-05-29 21:58:02 -0500

    Feature: Cambios en la Entidad Facultad y Rubro
    - Se agregan seeders con data de ejemplo
    - Se renombre la columna Rubro.nombre a Rubro.Descripcion para normalizar la base de datos
    - Se agregan las respectivas migraciones para modificar la BD

---
+++ ProyectoPOE/Datos/Migraciones/20250530023619_EditColumnDescripcionFacultad.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoPOE.Datos.Migraciones
+{
+    /// <inheritdoc />
+    public partial class EditColumnDescripcionFacultad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "nombre",
+                table: "facultad",
+                newName: "descripcion");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "descripcion",
+                table: "facultad",
+                newName: "nombre");
+        }
+    }
+}
